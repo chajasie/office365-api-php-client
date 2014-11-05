33038d9539fd87b9b72b3f624853b536856274ce
RonaldAus der Au <r.ausderau@gmail.com>
2014-11-05 14:17:17 +0100

    aufräumen

--- src/config.php
+++ src/config.php
@@ -1,8 +1,8 @@
 <?php
 global $apiConfig;
 $apiConfig = array(
-	'oauth2_client_id' => '',
-	'oauth2_secret' => '',  // Generatea key from Azure Management Portal
+	'oauth2_client_id' => '1',
+	'oauth2_secret' => 'test',  // Generatea key from Azure Management Portal
 	'oauth2_redirect' => 'http://localhost/office365-api-php-client/examples/oauth2.php',   //example url
 	'state' => '5fdfd60b-8457-4536-b20f-fcb658d19458'  //any unquiue key to Check against CSRF attack
 
